7b443770a964b29a76d5860a271bc3eed3c0c095
ao627515 <ao627515@gmail.com>
2025-10-01 15:48:04 +0000

    `Added new layout and styling for header component, including right-aligned icons and user information.`

--- src/views/components/src/header/index.tsx
+++ src/views/components/src/header/index.tsx
@@ -1,10 +1,28 @@
 import React from "react";
+import { Mail, CircleUserRound } from "lucide-react";
 import { SearchBar } from "../../index";
 
 function Header() {
   return (
     <header className="sac_header">
       <SearchBar />
+      <div className="sac_header__right">
+        <div className="sac_header__right__icon sac_header__icon_container">
+          <Mail />
+        </div>
+
+        <div className="sac_header__right__user">
+          <div className="sac_header__right__user__avatar sac_header__icon_container">
+            <CircleUserRound />
+          </div>
+          <div className="sac_header__right__user__info">
+            <h3 className="sac_header__right__user__info__name">John Doe</h3>
+            <p className="sac_header__right__user__info__email">
+              example@gmail.com
+            </p>
+          </div>
+        </div>
+      </div>
     </header>
   );
 }
